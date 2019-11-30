fb40ee6aaa9ba3bbc5ca4bd83ff0894857badc99
IvanObyhvost <i.obyhvost.work@gmail.com>
2019-11-30 23:55:15 +0200

    create film model

---
+++ src/app/models/film/film.ts
@@ -0,0 +1,11 @@
+export class Film {
+    id: number;
+    title: string;
+    year: number;
+    rate: number;
+    country: string;
+    imageUrl: string;
+    genre: string;
+    director: string;
+    isFavorite: boolean;
+}
