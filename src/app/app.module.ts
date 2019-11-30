f8ff46921635ad63ee5b9db63e8c7b0ab3d17d32
IvanObyhvost <i.obyhvost.work@gmail.com>
2019-11-30 14:42:36 +0200

    add cardFilm component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { TopFilmsComponent } from './pages/top-films/top-films.component';
+import { CardFilmComponent } from './components/card-film/card-film.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     TopFilmsComponent,
+    CardFilmComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/card-film/card-film.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-film',
+  templateUrl: './card-film.component.html',
+  styleUrls: ['./card-film.component.scss']
+})
+export class CardFilmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
