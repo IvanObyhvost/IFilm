67156926d38f4fccf47d18333c408a375f3636e7
IvanObyhvost <i.obyhvost.work@gmail.com>
2019-11-30 20:28:17 +0200

    add favorite-film page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { TopFilmsComponent } from './pages/top-films/top-films.component';
 import { CardFilmComponent } from './components/card-film/card-film.component';
+import { FavoriteFilmsComponent } from './pages/favorite-films/favorite-films.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     TopFilmsComponent,
-    CardFilmComponent
+    CardFilmComponent,
+    FavoriteFilmsComponent
   ],
   imports: [
     BrowserModule
