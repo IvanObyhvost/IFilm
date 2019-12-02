de8fc49aa3738890e1c94af1d222066bf24ec63f
Obyhvost Ivan <iv@yourserveradmin.com>
2019-12-02 19:13:27 +0200

    change favoriteFilms from object to number arr

--- src/app/services/store/store.service.ts
+++ src/app/services/store/store.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnInit } from '@angular/core';
 import { Film } from 'src/app/models/film/film';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { FilmService } from '../film/film.service';
 import { map } from 'rxjs/operators';
 const key = 'favoriteFilms';
@@ -8,12 +8,10 @@ const key = 'favoriteFilms';
   providedIn: 'root'
 })
 export class StoreService {
-  private favoriteFilms: Film[] = [];
-  private favoriteFilmsEvent = new BehaviorSubject<Film[]>([]);
   private films: Film[] = [];
   private filmsEvent = new BehaviorSubject<Film[]>([]);
-  private isLoading = new BehaviorSubject<boolean>(false);
-  constructor(private filmService: FilmService) { 
+  private favoriteFilms: number[] = [];
+  constructor(private filmService: FilmService) {
     this.getFavoriteFilmsFromLocalStorage();
   }
   private getFavoriteFilmsFromLocalStorage() {
@@ -23,48 +21,24 @@ export class StoreService {
     } catch (error) {
       this.favoriteFilms = [];
     }
-    this.favoriteFilmsEvent.next(this.favoriteFilms);
   }
 
   getTopFilms(limit: number) {
     return this.filmService.get(limit).pipe(
       map(response => {
         this.films = response.map(film => {
-          film.isFavorite = this.favoriteFilms.some(favoriteFilm => 
-            favoriteFilm.ranking === film.ranking);
+          film.isFavorite = this.favoriteFilms.some(ranking => ranking === film.ranking);
           return film;
         });
         return this.films;
       })
-    )
+    );
   }
   get FavoriteFilms() {
-    return this.favoriteFilmsEvent;
-  }
-
-  toggleFavoriteFilm(ranking: number) {
-    const film = this.films.find(film => film.ranking === ranking);
-    if (film) {
-      const favoriteFilm = this.favoriteFilms.find(film => film.ranking === ranking);
-      if (favoriteFilm) {
-        this.favoriteFilms = this.favoriteFilms.filter(film => film.ranking !== ranking);
-        film.isFavorite = false;
-      } else {
-        this.favoriteFilms = [...this.favoriteFilms, film];
-        film.isFavorite = true;
-      }
-      localStorage.setItem(key, JSON.stringify(this.favoriteFilms));
-      this.favoriteFilmsEvent.next(this.favoriteFilms);
-      this.filmsEvent.next(this.films);
-    }
-  }
-
-  setIsLoading(value: boolean) {
-    this.isLoading.next(value);
-  }
-
-  get IsLoading() {
-    return this.isLoading;
+    return this.filmsEvent.pipe(
+      map(films => films.filter(f => this.favoriteFilms.includes(f.ranking)))
+      // filter((film: Film[], index: number) => this.favoriteFilms.includes(film[index].ranking))
+    );
   }
 
   get Films() {
@@ -75,4 +49,20 @@ export class StoreService {
     this.films = films;
     this.filmsEvent.next(films);
   }
+
+  toggleFavoriteFilm(ranking: number) {
+    const selectedFilm = this.films.find(film => film.ranking === ranking);
+    if (selectedFilm) {
+      const favoriteFilm = this.favoriteFilms.find(favoriteRanking => favoriteRanking === ranking);
+      if (favoriteFilm) {
+        this.favoriteFilms = this.favoriteFilms.filter(favoriteRanking => favoriteRanking !== ranking);
+        selectedFilm.isFavorite = false;
+      } else {
+        this.favoriteFilms = [...this.favoriteFilms, selectedFilm.ranking];
+        selectedFilm.isFavorite = true;
+      }
+      localStorage.setItem(key, JSON.stringify(this.favoriteFilms));
+    }
+  }
+
 }
