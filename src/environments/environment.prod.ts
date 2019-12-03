75119c1a53f4b9cb508402a584643c3daf9c1ebc
Obyhvost Ivan <iv@yourserveradmin.com>
2019-12-03 12:01:03 +0200

    add values to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://www.myapifilms.com/imdb/',
+  token: '22c5dbab-403f-4040-9783-22b97f67a7fc',
+  directorLink: 'https://www.imdb.com/name/'
 };
