bc5f7fbb24b0b76a353417e0bacb9695fe3b1e63
Obyhvost Ivan <iv@yourserveradmin.com>
2019-12-03 18:10:07 +0200

    validate for join pipe

--- src/app/pipes/join/join.pipe.ts
+++ src/app/pipes/join/join.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { isArray } from 'util';
 
 @Pipe({
   name: 'join'
 })
 export class JoinPipe implements PipeTransform {
-  transform(values: any[], separator: string = ''): any {
+  transform(values: any[], separator: string = ''): string {
+    if (!isArray(values)) {
+      throw new Error('It is not array');
+    }
     return values.join(separator);
   }
 }
