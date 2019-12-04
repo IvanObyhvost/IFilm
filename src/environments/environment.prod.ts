fd80aa8b053bbb8bdc862ea79d9726a5c85fd25e
Obyhvost Ivan <iv@yourserveradmin.com>
2019-12-04 10:47:41 +0200

    update env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
-  api: 'https://www.myapifilms.com/imdb/',
+  api: 'https://www.myapifilms.com/',
   token: '22c5dbab-403f-4040-9783-22b97f67a7fc',
-  directorLink: 'https://www.imdb.com/name/'
+  directorLink: 'https://www.imdb.com/name/',
+  api_themoviedb: 'https://api.themoviedb.org/3/',
+  api_key: '9d2bff12ed955c7f1f74b83187f188ae',
+  image_url: 'https://image.tmdb.org/t/p/w200/'
 };
